62bac0f84e2e1cdfdb0a4d4df7ed0a22465d7bbb
Stefan Buck <stefanb@brandwatch.com>
2020-01-21 22:22:01 +0100

    fixup! add custom reporter

--- json-reporter.js
+++ json-reporter.js
@@ -5,16 +5,17 @@ class MyCustomReporter {
     }
   
     onRunComplete(contexts, results) {
+        results.testResults[0].testResults.forEach(item => {
+       if (item.status !== 'failed') {
+            return;
+        }
         const name = 'some.test.js';
         const line = 7;
         const col = 17;
-        const message = 'will it work or \n fail?';
+        const message = item.failureMessages[0];
         console.log('')
         console.log(`::error file=${name},line=${line},col=${col}::${message}`)
-        
-        const message2 = `foo 
-bar`;
-        console.log(`::error file=${name},line=${line+1},col=${col}::${message2}`)
+      })
     }
   }
   
