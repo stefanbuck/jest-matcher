824500374ca0b0c750a93068b4817a2878b43730
Stefan Buck <stefanb@brandwatch.com>
2020-04-24 12:47:04 +0200

    Add custom reporter to produce Problem Matcher ouput

---
+++ json-reporter.js
@@ -0,0 +1,30 @@
+class MyCustomReporter {
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    this._options = options;
+  }
+
+  onRunComplete(contexts, results) {
+    results.testResults[0].testResults.forEach((item) => {
+      if (item.status !== "failed") {
+        return;
+      }
+      const newLine = "%0A";
+      const name = results.testResults[0].testFilePath;
+
+      if (!item.location) {
+        throw new Error(
+          "You need to invoked jest with argument --testLocationInResults"
+        );
+      }
+
+      const line = item.location.line + 1;
+      const col = item.location.column;
+      const message = item.failureMessages[0].replace(/\n/g, newLine);
+
+      console.log(`::error file=${name},line=${line},col=${col}::${message}`);
+    });
+  }
+}
+
+module.exports = MyCustomReporter;
--- some.test.js
+++ some.test.js
@@ -2,4 +2,8 @@ describe("foo", function() {
   it("passes", function() {
     expect("a").toBe("a");
   });
+  
+  it("fails", function() {
+    expect("a").toBe("b");
+  });
 });
